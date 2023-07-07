109959f946be7362507590fbb4c22380389f3fa6
Serdar Mengütay <serdarmngty03@gmail.com>
2023-07-07 10:39:33 +0300

    pet adoption app created

---
+++ src/views/screens/DetailsScreen.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { SafeAreaView, View, Text, } from 'react-native'
+const DetailsScreen = () => {
+  return (
+    <SafeAreaView>
+      <Text>DetailsScreen</Text>
+    </SafeAreaView>
+  )
+}
+
+export default DetailsScreen
